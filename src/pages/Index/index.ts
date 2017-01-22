a0e5a8bdaf12c994893f8a4669febbed47fc9568
Marcin Chalecki <alcaster@op.pl>
2017-01-22 22:23:23 +0100

    Working login request. Returning token. No saving it, got to move as the separate provider.

--- src/pages/Index/index.ts
+++ src/pages/Index/index.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 import {NavController, ModalController, ViewController, NavParams, Platform} from 'ionic-angular';
+import {Http, Headers} from "@angular/http";
 
 @Component({
   selector: 'page-page1',
@@ -34,17 +35,51 @@ export class Index {
 <ion-content>
     <ion-list>
       <ion-item>
-      <!--here will be forms-->
-        <h1>Please enter login and password</h1>
+        <h2 style="text-align:center">Please enter login and password</h2>
       </ion-item>
+      <form (ngSubmit)="logForm()">
+        <ion-item>
+          <ion-label color="primary" floating>Login</ion-label>
+          <ion-textarea name="login" [(ngModel)]="account.login"></ion-textarea>
+        </ion-item>
+         <ion-item>
+          <ion-label color="primary" floating>Password</ion-label>
+          <ion-textarea name="password" [(ngModel)]="account.password"></ion-textarea>
+        </ion-item>
+        <ion-item>
+          <button class="center" ion-button type="submit" block (click)="dismiss()">
+            Save and login
+            </button>
+        </ion-item>
+      </form>
   </ion-list>
 </ion-content>
 `
 })
 
 export class ModalContentLogin {
-  constructor(public platform: Platform, public params: NavParams, public viewCtrl: ViewController) {
 
+  constructor(public platform: Platform, public params: NavParams, public viewCtrl: ViewController, public http: Http) {
+
+  }
+
+  account = {};
+
+  logForm() {
+    let loginUrl='http://127.0.0.1:8000/api/api-token-auth/';
+    let body = 'username='+this.account['login']+'&password='+this.account['password'];
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    this.http
+      .post(loginUrl,
+        body, {
+          headers: headers
+        })
+      .subscribe(data => {
+        console.log(data)
+      }, error => {
+        console.log(JSON.stringify(error.json()));
+      });
   }
 
   dismiss() {
